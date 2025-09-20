3e64915c2d66b952d0c5b43bc8c721447054921d
njoroge26262-lang <njoroge.26262@students.kyu.ac.ke>
2025-09-20 10:19:30 +0300

    Add files via upload

---
+++ assignment 1.cxx
@@ -0,0 +1,18 @@
+//Program: Semester Units display
+//Author:    CT100/G/26262/25
+//Purpose: Displaying all units for this semester
+
+
+#include <stdio.h>
+int main()
+{
+    printf("1.Essentials of accounting\n");
+    printf("2.Mathematics for science\n");
+    printf("3.Fundamentals of ICT\n");
+    printf("4.Communication & information literature\n");
+    printf("5.Analogue Electronics\n");
+    printf("6.Introduction to programming and Algorithms\n");
+    printf("7.Computer systems and Organizations\n");
+    printf("8.Introduction to computer Applications\n");
+    return 0;
+}
